ec097fbdb79d630a87c5230094d30dc9b56be627
Artur Kalinowski <Artur.Kalinowski@cern.ch>
2022-09-23 15:20:15 +0200

    automatic commit

--- scripts/makeStudentsVersion.py
+++ scripts/makeStudentsVersion.py
@@ -19,13 +19,20 @@ print(colored("Switching to students branch.","blue"))
 students_branch_name = branch_name.replace("_solutions","")
 if not testRun:
     result = subprocess.run(["git", "checkout",students_branch_name], text=True, capture_output=True)
+    print(result.stderr)
 print(colored("Removing solutions blocks.","blue"))
 
-fileList = glob.glob('./*.ipynb')
-for aFile in fileList:
-    input_file_name = aFile
+result = subprocess.run(["git","diff","--name-only", branch_name, students_branch_name], text=True, capture_output=True)
+print(result.stderr)
+fileList = result.stdout.rstrip("\n").split("\n")
+
+for aFile_name in fileList:
+    input_file_name = aFile_name
     output_file = open("tmp.ipynb", "w")
-    result = subprocess.run(["awk", " /#BEGIN_SOLUTION/{p=1}/#END_SOLUTION/{p=0;print \"    \\\"...\\\\n\\\", \";next}!p", input_file_name],
+    subprocess.run(["git","restore", "--source",branch_name,"--",aFile_name], text=True, capture_output=True)
+    result = subprocess.run(["git","add",aFile_name], text=True, capture_output=True)
+    print(result.stderr)
+    "...\n", 
                             text=True, stdout=output_file)
     if not testRun:
         subprocess.run(["mv","tmp.ipynb",input_file_name])
